56c4df7e4bf7c169128f00fd57ca02eeed108a32
glaxyt <glaxyt@naver.com>
2025-06-19 01:17:59 +0900

    chore: Zone 영속성 엔티티 구현

---
+++ src/main/java/com/moti/backend/core/place/domain/entity/Zone.java
@@ -0,0 +1,24 @@
+package com.moti.backend.core.place.domain.entity;
+
+import com.moti.backend.global.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "zone")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class Zone extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "place_id", nullable = false)
+    private Place place;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+}
