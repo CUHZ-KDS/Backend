b09ba0854816f925a7facf26303ad6483286711e
g00hyun <rhkdgus1417@hufs.ac.kr>
2025-06-22 20:55:46 +0900

    feat: 등급 repository 연결

--- src/main/java/com/moti/backend/core/show/infrastructure/persistence/GradeRepository.java
+++ src/main/java/com/moti/backend/core/show/infrastructure/persistence/GradeRepository.java
@@ -1,10 +1,13 @@
 package com.moti.backend.core.show.infrastructure.persistence;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.moti.backend.core.show.domain.entity.Show;
+import com.moti.backend.core.show.domain.entity.Grade;
 
 @Repository
-public interface ShowRepository extends JpaRepository<Show, Long> {
+public interface GradeRepository extends JpaRepository<Grade, Long> {
+	List<Grade> findByShowId(Long showId);
 }
