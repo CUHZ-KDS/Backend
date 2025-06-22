c5596903f967cf5efaf0dfdd308f51df295e2448
g00hyun <rhkdgus1417@hufs.ac.kr>
2025-06-22 20:56:50 +0900

    feat: 등급, 공연 도메인 서비스 구현 및 커스텀 예외(주석)

---
+++ src/main/java/com/moti/backend/core/show/exception/ShowNotFoundException.java
@@ -0,0 +1,9 @@
+package com.moti.backend.core.show.exception;
+
+import com.moti.backend.global.type.StatusCode;
+
+// public class ShowNotFoundException extends BusinessException {
+// 	public ShowNotFoundException(String message) {
+// 		super(StatusCode.NOT_FOUND, message);
+// 	}
+// }
