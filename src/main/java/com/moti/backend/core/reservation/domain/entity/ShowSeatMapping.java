8b2e101346ee05d1caf32ca4397db5d9ccb443ea
glaxyt <glaxyt@naver.com>
2025-06-24 14:12:04 +0900

    refactor: ShowSeatMapping 영속성 엔티티 메서드 추가 - 정적 생성 메서드 추가 - 상태 변경 메서드 및 예외 메세지 추가

--- src/main/java/com/moti/backend/core/reservation/domain/entity/ShowSeatMapping.java
+++ src/main/java/com/moti/backend/core/reservation/domain/entity/ShowSeatMapping.java
@@ -2,7 +2,10 @@ package com.moti.backend.core.reservation.domain.entity;
 
 import com.moti.backend.core.place.domain.entity.Seat;
 import com.moti.backend.core.reservation.domain.type.SeatStatus;
+import com.moti.backend.core.show.domain.entity.Grade;
 import com.moti.backend.global.entity.BaseTimeEntity;
+import com.moti.backend.global.exception.BusinessException;
+import com.moti.backend.global.type.StatusCode;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -27,11 +30,29 @@ public class ShowSeatMapping extends BaseTimeEntity {
     @JoinColumn(name = "seat_id", nullable = false)
     private Seat seat;
 
-//    @ManyToOne(fetch = LAZY)
-//    @JoinColumn(name = "grade_id", nullable = false)
-//    private Grade grade;
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "grade_id", nullable = false)
+    private Grade grade;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "status", nullable = false)
     private SeatStatus status;
+
+    public void reserve() {
+        if (this.status != SeatStatus.HOLD && this.status != SeatStatus.AVAILABLE) {
+            throw new BusinessException(StatusCode.CONFLICT, "RESERVED 혹은 AVAILABLE 상태는 결제가 불가합니다. 현재 상태: " + this.status);
+        }
+        this.status = SeatStatus.RESERVED;
+    }
+
+    public void hold() {
+        if (this.status != SeatStatus.AVAILABLE) {
+            throw new BusinessException(StatusCode.CONFLICT, "HOLD 혹은 BOOKED 상태는 예매가 불가합니다. 현재 상태: " + this.status);
+        }
+        this.status = SeatStatus.HOLD;
+    }
+
+    public void release() {
+        this.status = SeatStatus.AVAILABLE;
+    }
 }
