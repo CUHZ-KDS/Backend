bd741ce0905d229daecfea6a45b8899bf68fb334
glaxyt <glaxyt@naver.com>
2025-06-24 14:15:16 +0900

    feat: ReservationJpaRespository 추가

---
+++ src/main/java/com/moti/backend/core/reservation/domain/service/ShowSeatLockService.java
@@ -0,0 +1,38 @@
+package com.moti.backend.core.reservation.domain.service;
+
+import com.moti.backend.core.reservation.domain.entity.ShowSeatMapping;
+import com.moti.backend.core.reservation.infrastructure.ShowSeatMappingRepository;
+import com.moti.backend.global.exception.BusinessException;
+import com.moti.backend.global.type.StatusCode;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ShowSeatLockService {
+
+    private final ShowSeatMappingRepository showSeatMappingRepository;
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public List<ShowSeatMapping> lockAndHoldShowSeats(List<Long> showSeatIds) {
+        List<Long> sortedShowSeatIds = showSeatIds.stream().sorted().toList();
+
+        List<ShowSeatMapping> showSeats = showSeatMappingRepository.findAllByIdWithPessimisticLock(sortedShowSeatIds);
+
+        if (showSeats.size() != sortedShowSeatIds.size()) {
+            throw new BusinessException(StatusCode.BAD_REQUEST, "예약할 수 없는 좌석이 포함되어있습니다.");
+        }
+
+        for (ShowSeatMapping showSeat : showSeats) {
+            showSeat.hold();
+        }
+
+        return showSeats;
+    }
+}
---
+++ src/main/java/com/moti/backend/core/reservation/infrastructure/ReservationRepository.java
@@ -0,0 +1,8 @@
+package com.moti.backend.core.reservation.infrastructure;
+
+import com.moti.backend.core.reservation.domain.entity.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {}
